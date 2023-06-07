fedba0cb8584eb7c5476135a70be51d8bd5b2dad
Mohamedfaye12 <fayemohamed540@gmail.com>
2023-06-07 15:39:38 +0000

    commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Récupère l'élément h1 avec l'ID "animated-text"
+var animatedText = document.getElementById("animated-text");
+
+// Applique la classe "animated-text" à l'élément h1
+animatedText.classList.add("animated-text");
